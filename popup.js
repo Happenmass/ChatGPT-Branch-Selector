42f46d08e594c0cdc4536a9e8e7ee1468d6c3a41
Happen <248977265@qq.com>
2023-07-24 22:28:11 +0800

    正在添加右键改变节点颜色功能

--- popup.js
+++ popup.js
@@ -15,31 +15,90 @@ chrome.storage.local.get('divTree', ({ divTree }) => {
     }
   });
   
-  // 渲染树形结构的函数
-  function renderDivTree(container, node) {
-    const divNode = document.createElement('div');
-    divNode.classList.add('node');
-  
-    // 将 tagInfo 内容作为 data-attribute 存储在节点上
-    divNode.setAttribute('data-tag-info', `${node.textContent}`);
+// 渲染树形结构的函数
+function renderDivTree(container, node) {
+  const divNode = document.createElement('div');
+  divNode.classList.add('node');
 
-    // 将 type 属性作为 data-attribute 存储在节点上
-    divNode.setAttribute('data-type', node.type);
-  
-    container.appendChild(divNode);
-  
-    if (node.children.length > 0) {
-      const line = document.createElement('div');
-      line.classList.add('line');
-      container.appendChild(line);
-  
-      const childrenContainer = document.createElement('div');
-      childrenContainer.classList.add('children-container');
-      container.appendChild(childrenContainer);
-  
-      node.children.forEach(childNode => {
-        renderDivTree(childrenContainer, childNode);
-      });
-    }
+  // 将 tagInfo 内容作为 data-attribute 存储在节点上
+  divNode.setAttribute('data-tag-info', `${node.textContent}`);
+
+  // 将 type 属性作为 data-attribute 存储在节点上
+  divNode.setAttribute('data-type', node.type);
+
+  container.appendChild(divNode);
+
+  if (node.children.length > 0) {
+    const line = document.createElement('div');
+    line.classList.add('line');
+    container.appendChild(line);
+
+    const childrenContainer = document.createElement('div');
+    childrenContainer.classList.add('children-container');
+    container.appendChild(childrenContainer);
+
+    node.children.forEach(childNode => {
+      renderDivTree(childrenContainer, childNode);
+    });
   }
+}
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// 鼠标右键点击事件监听
+document.addEventListener('DOMContentLoaded', () => {
+  const nodes = document.querySelectorAll('.node');
+  logToBackground('nodescount'+nodes.length);
+  nodes.forEach(node => {
+    node.addEventListener('mousedown', event => {
+      if (event.button === 2) {
+        showContextMenu(event, node);
+      }
+    });
+  });
+});
+
+// 显示上下文菜单
+function showContextMenu(event, target) {
+  const contextMenu = document.createElement('div');
+  contextMenu.classList.add('context-menu');
+  contextMenu.style.top = `${event.clientY}px`;
+  contextMenu.style.left = `${event.clientX}px`;
+
+  const colors = ['star', 'bad', 'abandon']; // 这里可以添加更多颜色选项
+
+  colors.forEach(color => {
+    const menuItem = document.createElement('div');
+    menuItem.classList.add('menu-item');
+    menuItem.textContent = color;
+    menuItem.addEventListener('click', () => {
+      setNodeColor(target, color);
+      hideContextMenu();
+    });
+    contextMenu.appendChild(menuItem);
+  });
+
+  document.body.appendChild(contextMenu);
+
+  // 点击其他地方隐藏上下文菜单
+  document.addEventListener('click', hideContextMenu);
+
+}
+
+function logToBackground(message) {
+  chrome.runtime.sendMessage({ action: 'log', message: message });
+}
+
+
+// 隐藏上下文菜单
+function hideContextMenu() {
+  const contextMenu = document.querySelector('.context-menu');
+  if (contextMenu) {
+    contextMenu.remove();
+  }
+}
+
+// 设置节点颜色
+function setNodeColor(node, color) {
+  node.setAttribute('data-type', color);
+}
   
